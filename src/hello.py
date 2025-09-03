33bc6cb22069c47df4d6d690d140727b2f1b0f5b
Ravikumar Pisupati <ravikumar.pisupati@cloudfabrix.com>
2025-09-03 14:25:44 -0700

    Printing hello using different python env

--- src/hello.py
+++ src/hello.py
@@ -1,8 +1,8 @@
 
-Hello from Python inside GitHub Actions! 🚀
+Hello from Python inside GitHub Actions!
 Python version: 3.8.x
 ---
-Hello from Python inside GitHub Actions! 🚀
+Hello from Python inside GitHub Actions!
 Python version: 3.9.x
 ...
 
