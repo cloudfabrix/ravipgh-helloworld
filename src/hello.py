eebf235597acbc59816f520d8f2016888db4385b
Ravikumar Pisupati <ravikumar.pisupati@cloudfabrix.com>
2025-09-03 14:42:49 -0700

    One more try of matrix

--- src/hello.py
+++ src/hello.py
@@ -1,8 +1,2 @@
 
-Hello from Python inside GitHub Actions!
-Python version: 3.8.x
----
-Hello from Python inside GitHub Actions!
-Python version: 3.9.x
-...
-
+print("Hello from Python inside GitHub Actions!")
